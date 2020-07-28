2ac43036c07ece8a85d3b71fffb18588d9beb300
Mubarak Show <mubarak.show@gmail.com>
2020-07-28 22:42:18 +0100

    added few more APIs

--- functions/APIs/todos.js
+++ functions/APIs/todos.js
@@ -25,6 +25,22 @@ exports.getAllTodos = (request, response) => {
     })
 }
 
+exports.getTodoById = (req, res) => {
+  // if
+  db
+    .doc(`/todos/${req.params.todoId}`)
+    .get()
+    .then((todo) => {
+      if(!todo.exists) {
+        return res.status(404).json({ message: 'todo not found' })
+      }
+      return res.status(200).json(todo.data())
+    })
+    .catch((err) => {
+      return res.status(500).json({ message: 'error getting todo' })
+    })
+}
+
 exports.postOneTodo = (request, response) => {
   if (request.body.body.trim() === '') {
     return response.status(400).json({ body: 'Must not be empty' })
@@ -57,7 +73,7 @@ exports.postOneTodo = (request, response) => {
 }
 
 exports.deleteTodo = (request, response) => {
-  const document = db.doc(`/todo/${request.params.todoId}`);
+  const document = db.doc(`/todos/${request.params.todoId}`);
   document
     .get()
     .then((doc) => {
@@ -71,6 +87,21 @@ exports.deleteTodo = (request, response) => {
     })
     .catch((err) => {
       console.log('err', err)
+      return response.status(500).json({ error: err.code })
+    })
+}
+
+exports.editTodo = (request, response) => {
+  if(request.body.todoId || request.body.createdAt) {
+    response.status(403).json({ message: 'Not allowed to edit Title or Timestamp' });
+  }
+  let document = db.doc(`/todos/${request.params.todoId}`);
+  document
+    .update(request.body)
+    .then(() => {
+      response.json({ message: 'Updated todo successfully' })
+    })
+    .catch((err) => {
       return response.status(500).json({ error: err.code })
     })
 }
--- functions/index.js
+++ functions/index.js
@@ -4,12 +4,16 @@ const app = require('express')();
 
 const { 
   getAllTodos,
+  getTodoById,
   postOneTodo,
-  deleteTodo
+  deleteTodo,
+  editTodo
 } = require('./APIs/todos');
 
 app.get('/todos', getAllTodos);
+app.get('/todo/:todoId', getTodoById);
 app.post('/todo', postOneTodo);
-app.delete('/todo/:todoId', deleteTodo)
+app.delete('/todo/:todoId', deleteTodo);
+app.put('/todo/:todoId', editTodo);
 
 exports.api = functions.https.onRequest(app);
